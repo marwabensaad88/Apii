5ef06467e9f7004f9c1a2d712da39e94f82f273f
marwabensaad88 <marwabensaad88@yahoo.fr>
2020-08-30 17:47:51 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import UserList from "./UserList";
+function App() {
+  return (
+    <div>
+      <UserList />
+    </div>
+  );
+}
+export default App;
